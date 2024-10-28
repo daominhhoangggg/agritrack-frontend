97e7054ac732557b757f8a339a720671efd70692
Dao Minh Hoang <daominhhoangggg@gmail.com>
2024-10-28 20:01:03 +0700

    Contacts, Form and Invoices

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard';
 import Team from './scenes/team';
 import Contacts from './scenes/contacts';
-// import Invoices from './scenes/invoices';
-// import Form from './scenes/form';
+import Invoices from './scenes/invoices';
+import Form from './scenes/form';
 // import FAQ from './scenes/faq';
 import Bar from './scenes/bar';
 import Pie from './scenes/pie';
@@ -30,8 +30,8 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/* <Route path="/invoices" element={<Invoices />} /> */}
-              {/* <Route path="/form" element={<Form />} /> */}
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
               {/* <Route path="/faq" element={<FAQ />} /> */}
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
